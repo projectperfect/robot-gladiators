32bfa3b2044b036858a26c694a780e1ee714f8fa
projectperfect <jprfct@protonmail.com>
2023-01-21 05:19:26 -0800

    fighting mechanics implemented

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,11 +1,57 @@
 var playerName = window.prompt("What is your robot's name?");
 
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("Our robots name is " + playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-function fight() {
-  window.alert("The fight has begun!");
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-//fight();
+var fight = function () {
+  // alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
+
+  enemyHealth = enemyHealth - playerAttack;
+
+  console.log(
+    playerName +
+      " attacked " +
+      enemyName +
+      ". " +
+      enemyName +
+      " now has " +
+      enemyHealth +
+      " health remaining."
+  );
+
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
+  playerHealth = playerHealth - enemyAttack;
+
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
+
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+};
+
+fight();
