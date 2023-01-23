ed97e1224ac24870e6294f6573752ee7036da92e
projectperfect <jprfct@protonmail.com>
2023-01-22 21:09:27 -0800

    initial game setup

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,3 +1,13 @@
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
 var playerName = window.prompt("What is your robot's name?");
 var promptFight = window.prompt(
   "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
@@ -16,16 +26,49 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
       enemyHealth +
       " health remaining."
   );
+
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
+  // remove player's health by subtracting the amount set in the enemyAttck variable
+
+  playerHealth = playerHealth - enemyAttack;
+  console.log(
+    enemyName +
+      " attacked " +
+      playername +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
+
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+
+  // if player chooses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+  var confirmSkip = window.confirm("Are you sure you'd like  to quit?");
+  // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // substract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
+  } else {
+    fight();
+  }
+} else {
+  window.alert("You need to choose a valid option. Try again!");
 }
-
-var playerHealth = 100;
-var playerAttack = 10;
-
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
-var enemyHealth = 50;
-var enemyAttack = 12;
 
 var fight = function () {
   // alert players that they are starting the round
