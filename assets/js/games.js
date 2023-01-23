c48bd134953d64b97c2f32bc029d359ca56486ed
projectperfect <jprfct@protonmail.com>
2023-01-22 20:47:09 -0800

    condition added for the player to fight or skip fight

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,4 +1,22 @@
 var playerName = window.prompt("What is your robot's name?");
+var promptFight = window.prompt(
+  "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
+);
+
+if (promptFight === "fight" || promptFight === "FIGHT") {
+  // remove enemy's health by subtracting the amount set in the playerAttack variable
+  enemyHealth = enemyHealth - enemyAttack;
+  console.log(
+    playerName +
+      "attacked " +
+      enemyName +
+      ". " +
+      enemyName +
+      " now has" +
+      enemyHealth +
+      " health remaining."
+  );
+}
 
 var playerHealth = 100;
 var playerAttack = 10;
