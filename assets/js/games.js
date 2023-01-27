b304beeca088a07b9d70a344735836907d81b003
projectperfect <jprfct@protonmail.com>
2023-01-26 20:54:58 -0800

    start and end game functionality

--- assets/js/games.js
+++ assets/js/games.js
@@ -122,6 +122,20 @@ var startGame = function () {
     }
   }
 
+  var endGame = function () {
+    // if player is still alive, player wins!
+
+    if (playerHealth > 0) {
+      window.alert(
+        "Great job, you've survived the game! You now have a score " +
+          playerMoney +
+          "."
+      );
+    } else {
+      window.alert("You've lost your robot in battle.");
+    }
+  };
+
   // function to end the entire game
   var endGame = function () {
     window.alert("The game has now ended. Let's see how you did!");
@@ -130,3 +144,14 @@ var startGame = function () {
   // play again
   startGame();
 };
+
+// ask the player if they'd like to play again
+
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
